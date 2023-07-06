0122b4f79422f4bfd945bf2bc29bb3f203f205ec
Seshu1148 <seshachalam.katuri@bluehublabs.net>
2023-07-06 12:38:49 +0530

    Add project files.

---
+++ WebApplication2/Controllers/WeatherForecastController.cs
@@ -0,0 +1,56 @@
+using Microsoft.AspNetCore.Mvc;
+using API.Services;
+using API.Models;
+using API.DataAccess.Repository;
+using API.Services.Services;
+using API.Services.Interfaces;
+
+namespace WebApplication2.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+        protected readonly IStateCityService StateCityService;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger, IStateCityService stateCityService)
+        {
+            _logger = logger;
+            StateCityService = stateCityService;
+        }
+
+        [HttpGet(Name = "GetWeatherForecast")]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+
+        [HttpGet]
+        [Route("GetStateCityDetails")]
+        public async Task<ActionResult<IReadOnlyList<StateCityDetails>>> GetCityStateDetailsAsync([FromQuery] int CountryMasterID)
+        {
+            _logger.LogInformation("Accessing {} action {} for CountryMasterID {}", nameof(WeatherForecastController), nameof(GetCityStateDetailsAsync), CountryMasterID);
+            var cityStateDetails = await StateCityService.GetCityStateDetailsAsync(CountryMasterID);
+            if (cityStateDetails != null && cityStateDetails.Count > 0)
+            {
+                return cityStateDetails.ToList();
+            }
+            else
+            {
+                return NotFound("State City Details not exists for Country : " + CountryMasterID);
+            }
+        }
+    }
+}
---
+++ WebApplication2/Startup.cs
@@ -0,0 +1,73 @@
+﻿using API.DataAccess.Interfaces;
+using API.DataAccess.Repository;
+using API.Services.Config;
+using API.Services.Interfaces;
+using API.Services.Services;
+
+namespace WebApplication2
+{
+    public class Startup
+    {
+        public IConfiguration Configuration { get; }
+        public Startup(IConfiguration configuration)
+        {
+            this.Configuration = configuration;
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services
+
+                // Repositories
+                .AddScoped<ICityMasterRepository, CityMasterRepository>()
+                .AddScoped<ICountryMasterRepository, CountryMasterRepository>()
+                .AddScoped<IEmailScheduleRepository, EmailScheduleRepository>()
+                .AddScoped<IFundRepository, FundRepository>()
+                .AddScoped<IQuestionAnswerRepository, QuestionAnswerRepository>()
+                .AddScoped<IQuestionBankRepository, QuestionBankRepository>()
+                .AddScoped<IStateMasterRepository, StateMasterRepository>()
+                .AddScoped<IUserBankDetailsRepository, UserBankDetailsRepository>()
+                .AddScoped<IUserRepository, UserRepository>()
+                .AddScoped<IUserProfileRepository, UserProfileRepository>()
+                .AddScoped<IUserRoleRepository, UserRoleRepository>()
+                .AddScoped<IFundSubscriptionRepository, FundSubscriptionRepository>()
+                .AddScoped<IMasterDataRepository, MasterDataRepository>()
+                .AddScoped<IAzureBlobStorageActionsRepository, AzureBlobStorageActionsRepository>()
+
+
+                // Services
+                .AddScoped<ICountryMasterService, CountryMasterService>()
+                .AddScoped<IEmailService, EmailService>()
+                .AddScoped<IFundService, FundService>()
+                .AddScoped<IQuestionBankService, QuestionBankService>()
+                .AddScoped<IStateCityService, StateCityService>()
+                .AddScoped<IUserBankDetailsService, UserBankDetailsService>()
+                .AddScoped<IUserService, UserService>()
+                .AddScoped<IUserProfileService, UserProfileService>()
+                .AddScoped<IUserRoleService, UserRoleService>()
+                .AddScoped<IFundSubscriptionService, FundSubscriptionService>()
+                .AddScoped<IMasterDataService, MasterDataService>()
+
+                .Configure<AppSettings>(Configuration.GetSection("AppSettings"))
+
+                .AddCors(options =>
+                {
+                    options.AddPolicy("AllowOrigin", builder =>
+                    {
+                        builder
+                        .AllowAnyOrigin()
+                        .AllowAnyMethod()
+                        .AllowAnyHeader();
+                    });
+                });
+        }
+
+        public void Configure(WebApplication app, IWebHostEnvironment env)
+        {
+            app.UseStaticFiles();
+            app.UseCors("AllowOrigin");
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+    }
+}
